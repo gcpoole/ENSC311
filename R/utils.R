6e9bd477a0a558dfa1c83b18d5cc0be02ea7b1f0
gcpoole <gpoole@eco-metrics.com>
2025-01-18 09:44:26 -0700

    Move githb key to persistR file for a bit more safety...

--- R/utils.R
+++ R/utils.R
@@ -6,22 +6,17 @@
 #' @export
 update_ENSC311 <- function(instructor) {
 
-  # generate with utf8toint()
-  hsh = c(103, 104, 112, 95, 106, 114,  90,  68, 120,  79,  73, 86,  72,
-          50,  99, 120, 116, 112, 118,  98,  66,  57, 53, 120,  56,  71,  70,
-          57, 105,  88,  78, 118, 113, 121,  48,  50,  71,  78,  53, 110)
-
   if(!instructor %in% c("Poole","Felton"))
     stop(
       "\n`", instructor, "` is not a valid instructor number\n",
       'Valid instructors are "Poole" or "Felton"')
-  remotes::install_github("gcpoole/tutorialize", "HEAD", NULL, intToUtf8(hsh))
-  remotes::install_github("gcpoole/ENSC311", "HEAD", NULL, intToUtf8(hsh))
-  remotes::install_github("gcpoole/Tutorials311Shared", "HEAD", NULL, intToUtf8(hsh))
+  remotes::install_github("gcpoole/tutorialize", "HEAD", NULL, persistR::getPersistent("git_key", filename="~/R/persistR"))
+  remotes::install_github("gcpoole/ENSC311", "HEAD", NULL, persistR::getPersistent("git_key", filename="~/R/persistR"))
+  remotes::install_github("gcpoole/Tutorials311Shared", "HEAD", NULL, persistR::getPersistent("git_key", filename="~/R/persistR"))
   if(instructor == "Poole")
-    remotes::install_github("gcpoole/Tutorials311Poole","HEAD", NULL, intToUtf8(hsh))
+    remotes::install_github("gcpoole/Tutorials311Poole","HEAD", NULL, persistR::getPersistent("git_key", filename="~/R/persistR"))
   else
-    remotes::install_github("felt0134/R_Basics", "HEAD", NULL, intToUtf8(hsh))
+    remotes::install_github("felt0134/R_Basics", "HEAD", NULL, persistR::getPersistent("git_key", filename="~/R/persistR"))
   cat("\n")
   cat("===== NOTE!!! ===========\n")
   cat("= Be sure all files are saved.  Then, restart R from the Session menu.\n")
